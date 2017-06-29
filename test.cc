40f8a014ece3a00dc76538d804543e4f4416ee5f
Dominic Hamon <dominichamon@users.noreply.github.com>
2017-06-29 07:53:33 -0700

    Initial check in of a simple framework for a game
    generated assembly:
    
    `
    main:                                   # @main
            mov     al, 12
            jmp     .LBB0_1
    .LBB0_6:                                #   in Loop: Header=BB0_1 Depth=1
            add     al, cl
            movzx   ecx, al
            shl     rcx, 5
            mov     byte ptr [rcx + 22530], 32
            mov     byte ptr [22942], 16
    .LBB0_1:                                # =>This Inner Loop Header: Depth=1
            movzx   edx, byte ptr [0]
            test    dl, 8
            jne     .LBB0_2
            and     dl, 4
            shr     dl, 2
            cmp     al, 2
            seta    cl
            and     cl, dl
            mov     dl, -2
            test    cl, cl
            je      .LBB0_6
            jmp     .LBB0_5
    .LBB0_2:                                #   in Loop: Header=BB0_1 Depth=1
            cmp     al, 22
            setb    cl
            mov     dl, 2
            test    cl, cl
            je      .LBB0_6
    .LBB0_5:                                #   in Loop: Header=BB0_1 Depth=1
            mov     ecx, edx
            jmp     .LBB0_6
    `

---
+++ test.cc
@@ -0,0 +1,104 @@
+#include <cstdint>
+
+namespace {
+  volatile uint8_t& memory(const uint16_t loc) {
+    return *reinterpret_cast<uint8_t*>(loc);
+  }
+
+  constexpr bool test_bit(const uint8_t data, const uint8_t bit) {
+    return (data & (1 << bit)) != 0;
+  }
+
+  uint16_t row_address(uint8_t row) {
+    return 16384 + ((row / 64) * 2048) + ((row % 8) * 256) + (((row / 8) % 8) * 32);
+  }
+
+  uint16_t pixel_address(uint8_t row, uint8_t col) {
+    return row_address(row) + col;
+  }
+
+  struct Joystick {
+    // TODO: figure out where in(31) is in memory.
+    static constexpr uint16_t JOYSTICK_ADDRESS = 0;
+
+    struct PortData {
+      uint8_t data;
+    };
+
+    Joystick()
+      : Joystick(PortData{memory(JOYSTICK_ADDRESS)}) {}
+
+     Joystick(const PortData d)
+       : right(test_bit(d.data, 0)),
+         left(test_bit(d.data, 1)),
+         down(test_bit(d.data, 2)),
+         up(test_bit(d.data, 3)),
+         fire(test_bit(d.data, 4)) {}
+
+      bool up, down, left, right, fire;
+  };
+    
+  struct Z80 {
+    enum Color: uint8_t {
+      BLACK = 0,
+      BLUE = 1,
+      RED = 2,
+      MAGENTA = 3,
+      GREEN = 4,
+      CYAN = 5,
+      YELLOW = 6,
+      WHITE = 7,
+    };
+
+    using PixelColor = uint8_t;
+
+    static constexpr uint16_t SCREEN_BASE = 16384;
+    static constexpr uint16_t SCREEN_COLOR_BASE = 22528;
+
+    // TODO: figure out where out (254) is in memory.
+    static constexpr uint16_t BORDER_ADDRESS = 100;
+
+    static PixelColor color(const Color ink, const Color paper,
+                            const bool bright, const bool flash) {
+      return PixelColor(
+          uint8_t(ink) + (uint8_t(paper) * 8) + ((bright?1:0) * 64) + ((flash?1:0) * 128));
+    }
+
+    volatile void set_color(const uint8_t x, const uint8_t y, const PixelColor c) {
+      memory(SCREEN_COLOR_BASE + x + (32*y)) = c;
+    }
+
+    volatile void border(const Color c) {
+      memory(BORDER_ADDRESS) = c;
+    }
+  };
+
+  struct Player {
+    Player(const uint8_t x, const uint8_t y, const Z80::PixelColor c)
+      : x(x), y(y), c(c) {}
+
+    uint8_t x, y;
+    const Z80::PixelColor c;
+  };
+}
+
+int main() {
+  Z80 z80;
+
+  Player p1(2, 12, Z80::color(Z80::Color::BLACK, Z80::Color::GREEN,
+                              false, false));
+
+  Player p2(30, 12, Z80::color(Z80::Color::BLACK, Z80::Color::RED,
+                               false, false));
+
+  while (true) {
+    if (const auto joy = Joystick(); joy.up) {
+      if (p1.y < 22) p1.y += 2;
+    } else if(joy.down) {
+      if (p1.y > 2) p1.y -= 2;
+    }
+    
+    z80.set_color(p1.x, p1.y, p1.c);
+    z80.set_color(p2.x, p2.y, p2.c);
+  }
+}
