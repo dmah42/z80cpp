a4eb4eb4edc0d7505c9c508d73ae62730565de66
Dominic Hamon <dma+github@stripysock.com>
2018-05-25 23:39:04 +0100

    It's aliiiiive

--- Makefile
+++ Makefile
@@ -1,21 +1,47 @@
 # clang++ -S -mllvm --x86-asm-syntax=intel test.cc 
 
-CXX=clang++-3.5
+CXX=clang++-5.0
 CXXFLAGS=-O3 -Wall -Werror -std=c++1z
+AS=sdasz80
+ASFLAGS=-xlos -g
+LD=sdcc
+LDFLAGS=-mz80 --nostdlib --code-loc 0x8032 --data-loc 0x8200 -Wl -b_HEADER=0x8000
+
+app.tap: app.bin
+	appmake +zx --binfile ./app.bin --org 32768
+
+app.bin: game.rel crt0.rel
+	$(LD) $(LDFLAGS) $^
+	makezxbin <game.ihx >app.bin
+
+%.rel: %.z80
+	$(AS) $(ASFLAGS) $(basename $*).rel $*.z80
+
+crt0.rel: crt0.s
+	$(AS) $(ASFLAGS) crt0.rel crt0.s
 
 game.z80: game.s x86z80
 	./x86z80 -in game.s
 
+game.s: game.cc
+	$(CXX) $(CXXFLAGS) -S -mllvm --x86-asm-syntax=intel game.cc
+
 x86z80: ./go/x86z80.go
 	go build -o x86z80 ./go
 
-game.s: game.cc
-	$(CXX) $(CXXFLAGS) -S -mllvm --x86-asm-syntax=intel game.cc
+.PHONY: clean run test
+
+run: app.tap
+	fuse-gtk --tap ./app.tap \
+		--interface1 --interface2 \
+		--kempston --kempston-mouse \
+		--machine 48 --graphics-filter hq2x \
+		# --debugger-command "br 0x8000"
 
 test: x86z80 ./go/x86z80_test.go
 	go test ./go
 	./x86z80 -in test.asm
 
 clean:
-	-rm game.z80 game.s test.z80 ./x86z80
+	-rm app.tap app.bin *.rel game.z80 game.s game.map game.noi game.lk game.ihx *.lst *.sym test.z80 ./x86z80
 
--- go/x86z80.go
+++ go/x86z80.go
@@ -9,59 +9,84 @@ import (
 	"path"
 	"path/filepath"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
 var (
 	in = flag.String("in", "test.asm", "the file to read")
 
-	labelRE = regexp.MustCompile(`^\.?(\w+):`)
-	opRE = regexp.MustCompile(`^(\w+)\s+(.*)`)
+	labelRE     = regexp.MustCompile(`^(\.?\w+):`)
+	opRE        = regexp.MustCompile(`^(\w+)\s+(.*)`)
+	directiveRE = regexp.MustCompile(`^\s*(\.\w+)(\s+.+)?`)
 
 	// TODO: translate registers.
-	trMap = map[string]func(...string) []string {
-		"MOV": func(args ...string) []string {
-			return []string {
+	trMap = map[string]func(...string) []string{
+		"mov": func(args ...string) []string {
+			var ops []string
+			if _, err := strconv.Atoi(args[0]); err == nil {
+				args[0] = "#" + args[0]
+			} else if strings.HasPrefix(args[0], "byte ptr") {
+				addr := dectohex(args[0][10 : len(args[0])-1])
+				ops = append(ops, fmt.Sprintf("ld hl, #0x%s", addr))
+				args[0] = "(hl)"
+			}
+			if _, err := strconv.Atoi(args[1]); err == nil {
+				args[1] = "#" + args[1]
+			} else if strings.HasPrefix(args[1], "byte ptr") {
+				addr := dectohex(args[1][10 : len(args[0])-1])
+				ops = append(ops, fmt.Sprintf("ld hl, #0x%s", addr))
+				args[1] = "(hl)"
+			}
+			ops = append(ops, []string{
 				// Z80: LD loads the second arg into the first.
 				// Either arg can be a register, an 8bit integer, (HL) (the contents of register HL),
 				// or (IX + d) or (IY + d) where d is a two's complement integer offset.
-				// TODO: parse the args figure out which modes to use.
-				fmt.Sprintf("LD a, %s", args[1]),
-				fmt.Sprintf("LD %s, a", args[0]),
-			}
+				// fmt.Sprintf("ld a, %s", args[1]),
+				fmt.Sprintf("ld %s, %s", args[0], args[1]),
+			}...)
+			return ops
 		},
-		"AND": func(args ...string) []string {
-			return []string {
+		"and": func(args ...string) []string {
+			return []string{
 				// Z80: arg can be a register, a constant (8-bit), (HL), (IX+d), or (IY+d), d being 8-bit.
-				fmt.Sprintf("LD a, %s", args[0]),
-				fmt.Sprintf("AND %s", args[1]),
-				fmt.Sprintf("LD %s, a", args[0]),
+				fmt.Sprintf("ld a, %s", args[0]),
+				fmt.Sprintf("and %s", args[1]),
+				fmt.Sprintf("ld %s, a", args[0]),
 			}
 		},
-		"SHL": func(args ...string) []string {
-			return []string {
+		"shl": func(args ...string) []string {
+			return []string{
 				// Z80: arg can be the usual set.
-				fmt.Sprintf("LD a, %s", args[0]),
-				fmt.Sprintf("SLA a"),
-				fmt.Sprintf("LD %s, a", args[0]),
+				fmt.Sprintf("ld a, %s", args[0]),
+				fmt.Sprintf("sla a"),
+				fmt.Sprintf("ld %s, a", args[0]),
 			}
 		},
-		"SHR": func(args ...string) []string {
-			return []string {
+		"shr": func(args ...string) []string {
+			return []string{
 				// Z80: arg can be the usual set.
-				fmt.Sprintf("LD a, %s", args[0]),
-				fmt.Sprintf("SRA a"),
-				fmt.Sprintf("LD %s, a", args[0]),
+				fmt.Sprintf("ld a, %s", args[0]),
+				fmt.Sprintf("sra a"),
+				fmt.Sprintf("ld %s, a", args[0]),
 			}
 		},
-		"JMP": func(args ...string) []string {
-			return []string {
+		"jmp": func(args ...string) []string {
+			return []string{
 				// Z80: JP takes a 16-bit integer address or (HL), (IX), or (IY), unconditionally.
-				fmt.Sprintf("JP %s", args[0]),
+				fmt.Sprintf("jp %s", args[0]),
 			}
 		},
 	}
 )
+
+func dectohex(dec string) string {
+	addr, err := strconv.Atoi(dec)
+	if err != nil {
+		return fmt.Sprintf("<Unable to convert %q>", dec)
+	}
+	return fmt.Sprintf("%x", addr)
+}
 
 func stripComments(line string) string {
 	return strings.TrimSpace(strings.Split(line, "#")[0])
@@ -76,9 +101,9 @@ func formatOp(operator string, operands []string) (string, error) {
 		ops := op(operands...)
 		var trans string
 		if len(ops) != 0 {
-			trans = fmt.Sprintf("        %s\n", ops[0])
+			trans = fmt.Sprintf("\t%s\n", ops[0])
 			for _, o := range ops[1:] {
-				trans += fmt.Sprintf("        %s\n", o)
+				trans += fmt.Sprintf("\t%s\n", o)
 			}
 		}
 		return trans, nil
@@ -86,8 +111,29 @@ func formatOp(operator string, operands []string) (string, error) {
 	return "", fmt.Errorf("operator not found in mapping: %q", operator)
 }
 
+func formatDirective(directive string, params []string) (string, error) {
+	log.Println(directive, params)
+	// TODO: map
+	switch directive {
+	case ".intel_syntax", ".file", ".p2align", ".cfi_startproc", ".cfi_endproc", ".type":
+		return "", nil
+	case ".text":
+		return "\t.area\t_CODE\n", nil
+	case ".globl":
+		if len(params) != 1 {
+			return "", fmt.Errorf("expected param length 1, got %d", len(params))
+		}
+		return fmt.Sprintf("\t%s\t%s\n", directive, params[0]), nil
+	}
+
+	return "", fmt.Errorf("directive not found: %q", directive)
+}
+
 func translate(line string) (string, error) {
 	line = stripComments(line)
+	if len(line) == 0 {
+		return "", nil
+	}
 	maybeLabel := labelRE.FindStringSubmatch(line)
 	if maybeLabel != nil {
 		return formatLabel(maybeLabel[1]), nil
@@ -95,12 +141,25 @@ func translate(line string) (string, error) {
 
 	maybeOp := opRE.FindStringSubmatch(line)
 	if maybeOp != nil {
-		operator := strings.ToUpper(maybeOp[1])
+		operator := strings.ToLower(maybeOp[1])
 		operands := strings.Split(maybeOp[2], ",")
 		for i, o := range operands {
 			operands[i] = strings.TrimSpace(o)
 		}
 		return formatOp(operator, operands)
+	}
+
+	maybeDirective := directiveRE.FindStringSubmatch(line)
+	if maybeDirective != nil {
+		directive := strings.ToLower(maybeDirective[1])
+		var params []string
+		if len(maybeDirective) > 2 {
+			params = strings.Split(maybeDirective[2], ",")
+			for i, p := range params {
+				params[i] = strings.TrimSpace(p)
+			}
+		}
+		return formatDirective(directive, params)
 	}
 	return "", fmt.Errorf("line does not match regexp: %q", line)
 }
@@ -128,18 +187,21 @@ func main() {
 	}
 	defer outfile.Close()
 
-	fmt.Fprintf(outfile, "ORG 32768\n")
+	module := strings.TrimSuffix(path.Base(absin), path.Ext(*in))
+	fmt.Fprint(outfile, fmt.Sprintf("\t.module %s\n", module))
 
+	lineno := 1
 	scanner := bufio.NewScanner(infile)
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Fprint(outfile, fmt.Sprintf("# %s\n", line))
+		fmt.Fprint(outfile, fmt.Sprintf(";; %d: %s\n", lineno, line))
 		trans, err := translate(line)
 		if err != nil {
 			log.Printf(err.Error())
-			trans = fmt.Sprintf("# ERROR: %s\n", err)
+			trans = fmt.Sprintf(";; ERROR: %s\n", err)
 		}
 		fmt.Fprint(outfile, trans)
+		lineno += 1
 	}
 
 	if err := scanner.Err(); err != nil {
